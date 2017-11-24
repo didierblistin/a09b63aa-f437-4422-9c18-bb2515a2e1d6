b9cdf582da9ace56dfd733d1ed17b6ba471808cf
Didier Blistin <d.blistin@hotmail.com>
2017-11-24 12:26:50 +0100

    First commit

---
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public void TestForSonarQube()
+        {
+            int i = 0;
+            System.Diagnostics.Debug.WriteLine("Hello");
+        }
+
+    }
+}
